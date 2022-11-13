ae49ffbe1f824f4bc94d23dbbf4990b3b966dba5
Rodolfo Herrera Hernandez <codingwithrodi@gmail.com>
2022-11-13 10:21:54 -0300

    Update Home.jsx

--- Client/src/Pages/Everybody/Home/Home.jsx
+++ Client/src/Pages/Everybody/Home/Home.jsx
@@ -287,7 +287,7 @@ const HomePage = () => {
                                     onClick={(Event) => (Event.target.classList.contains('Recommendation-Letter-File') || (document.getElementById(Index).contains(Event.target))) && (SetRecommendationLetter(Data))}
                                     className='Recommendation-Letter-File'
                                 >
-                                    <div className='Basic-Info'>
+                                    <div className='Basic-Info' id={Index}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" version="1">
                                             <path
                                                 style={{
